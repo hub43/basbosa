152744390ec0d4e0d258ed49012b686346d332c3
Amir Mahmoud <nfutoam.atef@gmail.com>
2012-10-12 18:19:26 +0200

    Make special model called messages and make it collection is messages in database ,rename analytics_controller that in sockets to be messages_controller and adjust it to save the messages in messages collection , save all messages except network.ping

---
+++ cores/sockets/messages_controller.js
@@ -0,0 +1,24 @@
+var Messages = require ('../models/messages'), SocketServer = require ('../sockets/components/socket_server');
+
+var n = 10, queue = [], msg, messages;
+
+SocketServer.on('connection', function (socket) {
+	socket.lon ('*', function (e, message, next) {
+		msg = _.extend({}, message);
+		next();
+		if (!msg.internalMessage) {
+			Logger.info();
+			if(msg.eventName != 'network.ping') {
+				messages = new Messages;
+				messages.set("message", msg);
+				messages.set("timestamp", new Date().getTime());
+				queue.push(messages);
+				if (queue.length == n) {
+					queue[0].create (queue);
+					queue = [];
+				}
+			}
+		}
+	});
+});
+

00bb451daefc523a576a277a5ed48e2d79fdb5ad
Amir Mahmoud <nfutoam.atef@gmail.com>
2012-10-14 17:02:17 +0200

    documentation for countries_controller

--- cores/controllers/countries_controller.js
+++ cores/controllers/countries_controller.js
@@ -1,5 +1,19 @@
+/**
+ * CountryController is responsible on control the communication between Country Model 
+ * in the server side and the client side. 
+ * @module Cores
+ * @submodule CoresControllers
+ * @class CountryController
+ **/
+//define instance from country model.
 var Country = require('../models/country'), country = new Country;
 
+/**
+ * GetCountries is used to get the countries from database when the request is '/countries'.
+ * @method getCountries
+ * @param {String} req hold the request that come from the client. 
+ * @param {String} res the response that will hold the message to the client.
+ */
 exports.getCountries = function (req, res) {
 	var countryName = req.query['country'];
 	function callBack(error , docs) {

c67f87f596b4305f68323f23677fcf9a00055a58
Amir Mahmoud <nfutoam.atef@gmail.com>
2012-12-26 22:14:52 +0200

    Change validations_module.js methods signature and check they using in hawks and home

--- cores/libs/validations_module.js
+++ cores/libs/validations_module.js
@@ -18,66 +18,112 @@ define(['underscore'], function(_) {
 	};
 	var _public = {};
 	_public = {
-		isObjectId: function(msg, field) {
+		isObjectId: function(obj, field, options, cb) {
 			// Checks if field is an object id
 			// (i.e. a 12 byte hex number, taking into consideration that it's represented in a string)
 			// Check if field is a string (includes checking if it's present)
-			if (!_public.isString(msg, field)) return false;
+			if (typeof options === 'function') {
+		    cb = options;
+		    options = undefined;
+		  }
+			if (!_public.isString(obj, field)) {
+				if(cb !== undefined)	cb(false);
+				return false;
+			}
 			// Check if length is 24 characters (i.e. 12 bytes)
-			if (msg[field].length != 24) return false;
-			
+			if (obj[field].length != 24) {
+				if(cb !== undefined)	cb(false);
+				return false;
+			}
 			// checking that each character represents a hex numbers using a regular expression
-			return  /[\da-f]{24}/.test(msg[field]);	
+			if(cb !== undefined)	cb(/[\da-f]{24}/.test(obj[field]));
+			return  /[\da-f]{24}/.test(obj[field]);	
 		},
 		
-		isSideId: function(msg, field) {
+		isSideId: function(obj, field, options, cb) {
 			// Checks it field is a valid side id
-			
+			if (typeof options === 'function') {
+		    cb = options;
+		    options = undefined;
+		  }
 			// Check if field is a string (includes checking if it's present)
-			if (!_public.isString(msg, field))	return false;
+			if (!_public.isString(obj, field))	{
+				if(cb !== undefined)	cb(false);
+				return false;
+			}
 			// Check if length is 11 characters
-			if (msg[field].length != 11) return false;
+			if (obj[field].length != 11) {
+				if(cb !== undefined)	cb(false);
+				return false;
+			}
 			// Check if it follows the format 'side0-1-1-1' using a regular expression
 			var patt = /side[0-5]-[0-7]-[0-5]-[0-7]/;
-			return patt.test(msg[field]);
+			if(cb !== undefined)	cb(patt.test(obj[field]));
+			return patt.test(obj[field]);
 		},
 		
-		isPresent: function(msg, field) {
-			// Checks if field exists in a msg or not
-			return _.has(msg, field);
+		isPresent: function(obj, field, options, cb) {
+			// Checks if field exists in a obj or not
+			if (typeof options === 'function') {
+		    cb = options;
+		    options = undefined;
+		  }
+			if(cb !== undefined)	cb(_.has(obj, field));
+			return _.has(obj, field);
 		},
 
-		isString: function(msg, field) {
-			// Checks if field inside msg is a string or not
-			return _public.isPresent(msg, field) && _.isString(msg[field]);
+		isString: function(obj, field, options, cb) {
+			// Checks if field inside obj is a string or not
+			if (typeof options === 'function') {
+		    cb = options;
+		    options = {};
+		  }
+			if(cb !== undefined)	cb(_public.isPresent(obj, field) && _.isString(obj[field]));
+			return _public.isPresent(obj, field) && _.isString(obj[field]);
 		},
 
-		isInteger: function(msg, field) {
-			// Checks if field inside msg is an integer or not
-			return _public.isPresent(msg, field) && _.isNumber(msg[field]);
+		isInteger: function(obj, field, options, cb) {
+			// Checks if field inside obj is an integer or not
+			if (typeof options === 'function') {
+		    cb = options;
+		    options = {};
+		  }
+			if(cb !== undefined)	cb(_public.isPresent(obj, field) && _.isNumber(obj[field]));
+			return _public.isPresent(obj, field) && _.isNumber(obj[field]);
 		},
 
-		isURL: function(msg, field) {
-			// Checks if field inside msg is a url or not
-			return _public.isPresent(msg, field) && urlPattern.test(msg[field]);
+		isURL: function(obj, field, options, cb) {
+			// Checks if field inside obj is a url or not
+			if (typeof options === 'function') {
+		    cb = options;
+		    options = {};
+		  }
+			if(cb !== undefined)	cb(_public.isPresent(obj, field) && urlPattern.test(obj[field]));
+			return _public.isPresent(obj, field) && urlPattern.test(obj[field]);
 		},
 		/**
 		 * @method isLargerThan used to check if the length of any string  is larger than specific number.
-		 * @param {String} msg hold the message  that contain specific string that need t check on it.
+		 * @param {String} obj hold the message  that contain specific string that need t check on it.
 		 * @param {String} field hold the string that need to check on it.
 		 * @param {Number} min hold the number that need to limit the length of the string by it.
 		 */
-		isLargerThan: function(msg, field, min) {
-			// Checks if field inside msg is larger than the min value
-			if(min === undefined) {
-				min = field ;
-				field = msg;
-				msg = undefined;
+		isLargerThan: function(obj, field, options, cb) {
+			// Checks if field inside obj is larger than the min value
+			if (typeof options === 'function') {
+		    cb = options;
+		    options = undefined;
+		  } 
+			if(options === undefined) {
+				options = field ;
+				field = obj;
+				obj = undefined;
 			}
-			if(msg ===undefined) {
-				return (field.length >= min);
+			if(obj ===	undefined) {
+				if(cb !== undefined) cb((field.length >= options));
+				return (field.length >= options);
 			} else {
-				return _public.isPresent(msg, field) && (msg[field].length >= min);
+				if(cb !== undefined) cb(_public.isPresent(obj, field) && (obj[field].length >= options));
+				return _public.isPresent(obj, field) && (obj[field].length >= options);
 			}
 			
 		},
@@ -85,17 +131,52 @@ define(['underscore'], function(_) {
 		 * @method isValidMail used to check if the value valid to be email or not
 		 * @param {String} value hold the value  that need to check on it.
 		 */
-		isValidMail: function(value) {
-			return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(value)
+		isValidMail: function(obj, field, options, cb) {
+			if (typeof field === 'function') {
+		    cb = field;
+		    options	= {};
+		    field = {};
+		  }
+			if (typeof options === 'function') {
+		    cb = options;
+		    options = {};
+		  }
+			
+			if(cb !== undefined) {
+				cb(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(obj));
+			}
+			return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(obj);
 		},
-		isSmallerThan: function(msg, field, max) {
-			// Checks if field inside msg is smaller than the max value
-			return _public.isPresent(msg, field) && (msg[field].length <= max);
+		isSmallerThan: function(obj, field, options, cb) {
+			if (typeof options === 'function') {
+		    cb = options;
+		    options = undefined;
+		  }
+			if(options === undefined) {
+				options = field ;
+				field = obj;
+				obj = undefined;
+			}
+			if(obj ===	undefined) {
+				if(cb !== undefined) cb(_public.isPresent(obj, field) && (obj[field].length <= options));
+				return cb(_public.isPresent(obj, field) && (obj[field].length <= options));
+			} else {
+				if(cb !== undefined) cb(_public.isPresent(obj, field) && (obj[field].length <= options));
+				// Checks if field inside obj is smaller than the max value
+				return _public.isPresent(obj, field) && (obj[field].length <= options);
+			}
 		},
 
-		isWithin: function(msg, field, min, max) {
-			// Checks if field inside msg is within the min and max values
-			return _public.isSmallerThan(msg, field, max) && _public.isLargerThan(msg, field, min);
+		isWithin: function(obj, field, options, cb) {
+			// Checks if field inside obj is within the min and max values
+			if(typeof options === 'function') {
+				cb = options;
+				options = field;
+				field = obj;
+				obj = undefined;
+			}
+			if(cb !== undefined)	cb(_public.isSmallerThan(obj, field, options.max) && _public.isLargerThan(obj, field, options.min));
+			return _public.isSmallerThan(obj, field, options.max) && _public.isLargerThan(obj, field, options.min);
 		},
 		
 		validateWith: function(validator, msg) {
@@ -112,8 +193,8 @@ define(['underscore'], function(_) {
 
 				var validation_results = new Array();
 				_.each(validator, function(fields) {
-					_.each(fields, function(validators, key){
-						_.each(validators, function(obj){
+					_.each(fields, function(validators, key) {
+						_.each(validators, function(obj) {
 							// obj could be a function or an array that contains a function + extra paramters to be given
 							// while invoking that function later on in the validation process
 							// so first we check if it's an array

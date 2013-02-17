b2a575e0cf0619cf18f1b1536d3524d68036d794
Youssef Garas <ygaras@irisoftonline.com>
2013-02-17 23:42:47 +0200

    A lot of cleanup, more is still needed

--- cores/models/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Index Model it is just a class help to load anther files like user and sector 
- * @module Cores
- * @submodule CoresModels
- * @class Index
- **/
-	module.exports.User		      = require('./user');
-	module.exports.Sector		    = require('./sector');
-	module.exports.ClientModels = AppDirLoad(APP_PATH +'/node_modules/basbosa/corec/models');

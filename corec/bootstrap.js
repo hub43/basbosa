a688163f91737c39629edacbed1051063367694f
Youssef Garas <ygaras@hub43.com>
2013-11-27 14:18:42 -0500

    Using relative paths

--- corec/bootstrap.js
+++ corec/bootstrap.js
@@ -1,9 +1,9 @@
 require.config({
   paths : {
-    backbone : '/node_modules/basbosa/node_modules/basbosa-mongo/node_modules/backbone/backbone',
-    underscore : '/node_modules/basbosa/node_modules/underscore/underscore',
-    jquery : '/node_modules/basbosa/corec/vendors/jquery-2.0.3.min',
-    basbosa : '/node_modules/basbosa/node_modules/basbosa-registry/index'
+    backbone : '../node_modules/basbosa-mongo/node_modules/backbone/backbone',
+    underscore : '../node_modules/underscore/underscore',
+    jquery : './vendors/jquery-2.0.3.min',
+    basbosa : '../node_modules/basbosa-registry/index'
   },
   shim: {
     underscore : {
@@ -21,13 +21,15 @@ require.config({
     }
   }
 });
+
 define([
   'basbosa',
-  '/node_modules/basbosa/node_modules/basbosa-logger/index.js',
+  '../node_modules/basbosa-logger/index',
   'backbone',
   'underscore',
   'require'
-], function(B, Logger, Backbone, _, require) {
+
+], function(B, Logger, Backbone, _, require, app) {
   if (!B.added('Logger')) B.add('Logger', Logger);
   B.add('Backbone', Backbone);
   B.add('_', _);
